239ade7316950b9bc966779cc007bc9aae268110
Ale Bark Bruneri <ale.bark_bruneri@hotmail.com>
2019-06-19 00:56:35 -0300

    fixed values styles, images, paddings and stacks

--- lib/main.dart
+++ lib/main.dart
@@ -2,41 +2,59 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-      title: "People Counter",
-      home: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Text(
-            "Counter: 0",
-            style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              FlatButton(
-                child: Text(
-                  "Add someone",
-                  style: TextStyle(fontSize: 15.0, color: Colors.white),
+    title: "People Counter",
+    home: Stack(
+      children: <Widget>[
+        Image.asset("images/reception.png", fit: BoxFit.fitHeight, height: 1000.0),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
+              Padding(
+                  padding: EdgeInsets.fromLTRB(0, 45, 0, 295),
+                  child: Text(
+                    "Counter: 0",
+                    style: TextStyle(
+                        color: Colors.black, fontWeight: FontWeight.bold),
+                  )),
+            ]),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.fromLTRB(5, 5, 5, 38),
+                  child: FlatButton(
+                    child: Text(
+                      "Add someone",
+                      style: TextStyle(fontSize: 18.0, color: Colors.black),
+                    ),
+                    onPressed: () {},
+                  ),
                 ),
-                onPressed: () {},
-              ),FlatButton(
-                child: Text(
-                  "Remove someone",
-                  style: TextStyle(fontSize: 15.0, color: Colors.white),
+                Padding(
+                  padding: EdgeInsets.fromLTRB(5, 5, 5, 38),
+                  child: FlatButton(
+                    child: Text(
+                      "Remove someone",
+                      style: TextStyle(fontSize: 18.0, color: Colors.black),
+                    ),
+                    onPressed: () {},
+                  ),
                 ),
-                onPressed: () {},
-              ),
-            ],
-          ),
-          Text(
-            "Flutter Hotel",
-            style: TextStyle(
-              color: Colors.white,
-              fontWeight: FontWeight.bold,
-              fontStyle: FontStyle.italic,
-              fontSize: 25.0,
+              ],
             ),
-          )
-        ],
-      )));
+            Text(
+              "Flutter Hotel",
+              style: TextStyle(
+                color: Colors.black,
+                fontWeight: FontWeight.bold,
+                fontStyle: FontStyle.italic,
+                fontSize: 30.0,
+              ),
+            )
+          ],
+        )
+      ],
+    ),
+  ));
 }
